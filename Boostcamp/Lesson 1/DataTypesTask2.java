ddd9ca25b3d6b4394713877440affa3ae0b8a08e
Abdullah Alakbarli <154253374+abdullahalakbarli@users.noreply.github.com>
2025-02-03 22:03:21 +0400

    Create DataTypesTask2.java

---
+++ Boostcamp/Lesson 1/DataTypesTask2.java
@@ -0,0 +1,11 @@
+public class calculateRectangle {
+    public static int calculateArea(int a, int b){
+        System.out.println("Area of rectangle:");
+        return a * b;
+    }
+  
+    public static int calculatePerimeter(int a, int b){
+        System.out.println("Perimeter of rectangle:");
+        return 2 * (a + b);
+    }
+}
