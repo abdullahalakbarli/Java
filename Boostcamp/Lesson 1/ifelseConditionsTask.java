d79ee4ae2a979bbc2ba243ab4631f2cb280efa47
Abdullah Alakbarli <154253374+abdullahalakbarli@users.noreply.github.com>
2025-02-03 22:08:25 +0400

    Create ifelseConditionsTask.java

---
+++ Boostcamp/Lesson 1/ifelseConditionsTask.java
@@ -0,0 +1,13 @@
+public class checkNumber {
+    public static void check(int a){
+        if (a > 0){
+            System.out.println("Positive");
+        }
+        else if (a == 0){
+            System.out.println("Zero");
+        }
+        else {
+            System.out.println("Negative");
+        }
+    }
+}
