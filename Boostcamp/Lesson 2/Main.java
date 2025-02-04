4937b40dfab500bdd73133d373f8ec452b1a9e8a
Abdullah Alakbarli <154253374+abdullahalakbarli@users.noreply.github.com>
2025-02-04 22:47:58 +0400

    Update Main.java

--- Boostcamp/Lesson 2/Main.java
+++ Boostcamp/Lesson 2/Main.java
@@ -13,25 +13,29 @@ public class Main
         product.setProductPrice(1356.69);
         product.setDiscountPercentage(24);
         System.out.println("Discounted Price for " + product.getProductName() + ": $" + product.showDiscount());
-
+        product.addtoWishlist();
+        
         Computer product2 = new Computer();
         product2.setProductName("Zeiron Omen");
         product2.setProductPrice(389.54);
         product2.setDiscountPercentage(0);
         product2.setProductRating(4.4);
         System.out.println("Discounted Price for " + product2.getProductName() + ": $" + product2.showDiscount());
-
+        product2.addtoWishlist();
+        
         Computer product3 = new Computer();
         product3.setProductName("Apple Macbook");
         product3.setProductPrice(1641.7);
         product3.setDiscountPercentage(0);
         product3.setProductRating(4.6);
         System.out.println("Discounted Price for " + product3.getProductName() + ": $" + product3.showDiscount());
-
+        product3.addtoWishlist();
+        
         Computer product4 = new Computer();
         product4.setProductName("RaXius");
         product4.setProductPrice(922.29);
         product4.setDiscountPercentage(0);
         System.out.println("Discounted Price for " + product4.getProductName() + ": $" + product4.showDiscount());
+        product4.addtoWishlist();
     }
 }
